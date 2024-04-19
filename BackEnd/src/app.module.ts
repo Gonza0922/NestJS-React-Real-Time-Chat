b58aadbd27df4e8d0e236d0131fd4b5707277465
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-04-19 13:21:02 -0300

    feat: database and messages entity created

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { WebSocketsModule } from './websockets/websockets.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [WebSocketsModule]
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST_DB,
+      port: parseInt(process.env.PORT_DB),
+      username: process.env.USER_DB,
+      password: process.env.PASSWORD_DB,
+      database: process.env.NAME_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], //*TODO nuestra ruta + /carpeta/archuvo.entity.ts o js lo crea como tabla
+      synchronize: true,
+    }),
+    WebSocketsModule,
+  ],
 })
 export class AppModule {}
---
+++ BackEnd/src/messages/messages.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'messages' })
+export class Message {
+  @PrimaryGeneratedColumn()
+  message_ID: number;
+  @Column()
+  person: string;
+  @Column()
+  content: string;
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
