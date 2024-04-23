9267b635c6bcddcfd055b628e63758bd444d8fdb
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-04-23 16:11:30 -0300

    feat: auth guard created with token verification

--- BackEnd/src/auth/auth.controller.ts
+++ BackEnd/src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { LoginUserDto, RegisterUserDto } from '../users/users.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
   @Post('/register')
   registerUser(@Body() newUser: RegisterUserDto) {
     return this.authService.signUp(newUser);
@@ -12,5 +17,10 @@ export class AuthController {
   @Post('/login')
   loginUser(@Body() user: LoginUserDto) {
     return this.authService.signIn(user);
+  }
+  @Get('/verify')
+  @UseGuards(AuthGuard)
+  verifyUser(@Req() req: any) {
+    return this.usersService.getUser(req.user.user_ID);
   }
 }
--- BackEnd/src/auth/auth.module.ts
+++ BackEnd/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from 'src/users/users.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService],
 })
 export class AuthModule {}
