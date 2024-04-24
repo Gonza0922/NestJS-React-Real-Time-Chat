e280f9b90790a62d292d03c1d448fd5cdf7e94c5
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-04-24 00:42:44 -0300

    feat-frontend: Chat and NotFound pages created with its routes in the App.tsx using react-router-dom

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,78 +1,15 @@
-import { useState, useEffect, FormEvent } from "react";
-import io from "socket.io-client";
-import { getAllMessagesRequest } from "./api/messages.api.ts";
-
-const socket = io("http://localhost:3000");
-
-interface message {
-  person: string;
-  content: string;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Chat from "./pages/Chat";
 
 function App() {
-  const [text, setText] = useState("");
-  const [messages, setMessages] = useState<message[]>([]);
-
-  useEffect(() => {
-    const getAllMessages = async () => {
-      const data = await getAllMessagesRequest();
-      console.log(data);
-      setMessages([...messages, ...data]);
-    };
-    getAllMessages();
-  }, []);
-
-  useEffect(() => {
-    socket.on("message", (data: message) => {
-      setMessages((state: message[]) => [...state, data]);
-    });
-    return () => {
-      socket.off("message");
-    };
-  }, []);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (text.trim()) {
-      // no se envian inputs vacios
-      socket.emit("message", text);
-      setMessages([...messages, { person: "Me", content: text }]);
-      setText("");
-    }
-  };
-
   return (
-    <div className="container">
-      <nav className="navbar">Real Time Chat</nav>
-      <div className="screen">
-        {messages.map((message: message, index: number) =>
-          message.person === "Me" ? (
-            <div key={index} className="right">
-              <span className="person">{message.person}</span>
-              <p className="content">{message.content}</p>
-            </div>
-          ) : (
-            <div key={index} className="left">
-              <span className="person">{message.person}</span>
-              <p className="content">{message.content}</p>
-            </div>
-          )
-        )}
-      </div>
-      <div className="space"></div>
-      <form onSubmit={handleSubmit}>
-        <input
-          id="input"
-          value={text}
-          type="text"
-          onChange={(e) => setText(e.target.value)}
-          autoFocus
-          spellCheck
-          autoComplete="off"
-        />
-        <button>Send</button>
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
