b5aa71fcca610d0463747005c13bb6f3386ede4b
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-05-14 23:54:14 -0300

    feat-frontend: panel to config user profile created with styles, but without functionality

--- FrontEnd/src/pages/Chat.tsx
+++ FrontEnd/src/pages/Chat.tsx
@@ -5,9 +5,10 @@ import { useGetAllUsers } from "../hooks/users.hooks.ts";
 import { RegisterData } from "../interfaces/user.interfaces.ts";
 import { getDateAndHours } from "../functions/getDateAndHours.ts";
 import { useSocketContext } from "../contexts/SocketContext.tsx";
+import { useForm } from "react-hook-form";
 
 function Chat() {
-  const { user, logout } = useUserContext();
+  const { user, logout, error } = useUserContext();
   const {
     conectedUsers,
     socket,
@@ -21,13 +22,21 @@ function Chat() {
   } = useSocketContext();
   const { users } = useGetAllUsers(user.name);
   const [text, setText] = useState("");
+  const [panel, setPanel] = useState("chats");
+  const [updateName, setUpdateName] = useState(user.name);
   const scrollRef = useRef<HTMLDivElement>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterData>();
 
   useEffect(() => {
     if (scrollRef.current) scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
   }, [messages]);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const textHandleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (text.trim()) {
       // no se envian inputs vacios
@@ -58,6 +67,10 @@ function Chat() {
     return [content, resultSender, resultCreatedAt];
   };
 
+  const handleUpdateProfile = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
     <>
       <nav className="navbar">
@@ -67,42 +80,115 @@ function Chat() {
         </button>
       </nav>
       <div className="chats-panel">
-        <nav className="chats-navbar">Chats</nav>
-        <div className="chats" ref={scrollRef}>
-          {users.map((receiver: RegisterData, index: number) => {
-            const [lastMessageContent, lastMessageSender, lastMessageCreatedAt] = getLastMessage(
-              user.name,
-              receiver.name
-            );
-            return (
-              <div
-                key={index}
-                className={`sender-chat ${userToSend === receiver.name ? "selected" : ""}`}
-                onClick={() => setUserToSend(receiver.name)}
-              >
-                <div className="container-image-and-online">
-                  <div
-                    className={conectedUsers.includes(receiver.name) ? "online" : "offline"}
-                  ></div>
-                  <img className="user-image" src={receiver.image} alt="user-image" />
+        <nav className="chats-navbar">
+          <span onClick={() => setPanel("chats")} className="span-chats">
+            Chats
+          </span>
+          <img
+            onClick={() => setPanel("image")}
+            className="profile-image-mini"
+            src={user.image}
+            alt="profile Image"
+          />
+        </nav>
+        {panel === "chats" ? (
+          <div className="chats" ref={scrollRef}>
+            {users.map((receiver: RegisterData, index: number) => {
+              const [lastMessageContent, lastMessageSender, lastMessageCreatedAt] =
+                getLastMessage(user.name, receiver.name);
+              return (
+                <div
+                  key={index}
+                  className={`sender-chat ${userToSend === receiver.name ? "selected" : ""}`}
+                  onClick={() => setUserToSend(receiver.name)}
+                >
+                  <div className="container-image-and-online">
+                    <div
+                      className={conectedUsers.includes(receiver.name) ? "online" : "offline"}
+                    ></div>
+                    <img className="user-image" src={receiver.image} alt="user-image" />
+                  </div>
+                  <div className="container-user-chat-content">
+                    <span className="sender-chat-span">{receiver.name}</span>
+                    <p className="sender-content">
+                      {lastMessageSender === user.name ? "Me" : receiver.name}:{" "}
+                      {lastMessageContent &&
+                        (lastMessageContent.length < 25
+                          ? lastMessageContent
+                          : `${lastMessageContent.substring(0, 25)}...`)}
+                    </p>
+                    <span className="last-message-hour">
+                      {getDateAndHours(lastMessageCreatedAt)}
+                    </span>
+                  </div>
                 </div>
-                <div className="container-user-chat-content">
-                  <span className="sender-chat-span">{receiver.name}</span>
-                  <p className="sender-content">
-                    {lastMessageSender === user.name ? "Me" : receiver.name}:{" "}
-                    {lastMessageContent &&
-                      (lastMessageContent.length < 25
-                        ? lastMessageContent
-                        : `${lastMessageContent.substring(0, 25)}...`)}
-                  </p>
-                  <span className="last-message-hour">
-                    {getDateAndHours(lastMessageCreatedAt)}
-                  </span>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="update-profile">
+            {/* <div className="container-profile-image">
+              <img
+                className="profile-image"
+                src={user.image}
+                alt="profile Image"
+                onClick={() => console.log("Click")}
+              />
+              <button className="button-profile-image"></button>
+            </div> */}
+            <div className="container-input-and-profile-image">
+              <div className="input-and-profile-image">
+                <input type="file" />
+                <img
+                  src={user.image}
+                  alt="profile Image"
+                  className="profile-image"
+                  onClick={() => console.log("Click")}
+                />
+              </div>
+            </div>
+            <form onSubmit={handleUpdateProfile}>
+              <div className="container-errors">
+                {error === "User not found" ? (
+                  <div className="error">{error}</div>
+                ) : error === "Incorrect Password" ? (
+                  <div className="error">{error}</div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+              <div className="row-input">
+                <div className="input-field">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    id="name"
+                    type="text"
+                    value={updateName}
+                    className="validate"
+                    autoComplete="off"
+                    spellCheck={false}
+                    {...register("name", {
+                      required: { value: true, message: "File is required" },
+                      onChange: (e) => {
+                        setUpdateName(e.target.value);
+                      },
+                    })}
+                  />
+                  <div className="container-span">
+                    {errors.name && <span>{errors.name.message}</span>}
+                  </div>
                 </div>
               </div>
-            );
-          })}
-        </div>
+              <div className="row-input">
+                <div className="container-button-login-register">
+                  <button type="submit" id="reserve" className="button-login-register">
+                    Update Profile
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
       </div>
       {userToSend !== "none" ? (
         <div className="container">
@@ -130,7 +216,7 @@ function Chat() {
               )
             )}
           </div>
-          <form className="chat-form" onSubmit={handleSubmit}>
+          <form className="chat-form" onSubmit={textHandleSubmit}>
             <input
               className="input-chat"
               id="input"
