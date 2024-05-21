6ffdcbe69e1d6a27f60587a6853e89a89e54e9d0
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-05-21 00:11:11 -0300

    feat: endpoints created to get and create room

--- BackEnd/src/rooms/rooms.controller.ts
+++ BackEnd/src/rooms/rooms.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RoomsService } from './rooms.service';
+import { CreateRoomDto } from './rooms.dto';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+  constructor(private roomsService: RoomsService) {}
+  @Get('/get/:roomName')
+  getAllMessagesEndpoint(@Param('roomName') roomName: string) {
+    return this.roomsService.getRoomByName(roomName);
+  }
+  @Post('/post')
+  createMessageEndpoint(@Body() newRoom: CreateRoomDto) {
+    return this.roomsService.postRoom(newRoom);
+  }
+}
--- BackEnd/src/rooms/rooms.service.ts
+++ BackEnd/src/rooms/rooms.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Room } from './rooms.entity';
+import { Repository } from 'typeorm';
+import { CreateRoomDto } from './rooms.dto';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+  constructor(
+    @InjectRepository(Room) private roomRepository: Repository<Room>,
+  ) {}
+  async getRoomByName(roomName: string) {
+    const findRoom = await this.roomRepository.find({
+      relations: ['creator', 'member'],
+      where: { name: roomName },
+    });
+    return findRoom;
+  }
+  postRoom(newRoom: CreateRoomDto) {
+    const newRoomCreated = this.roomRepository.create(newRoom);
+    return this.roomRepository.save(newRoomCreated);
+  }
+}
